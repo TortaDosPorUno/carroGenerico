5584bdda8ab0b58eebba696f5116b8b4bc3cdfe3
TortaDosPorUno <65512807+TortaDosPorUno@users.noreply.github.com>
2020-10-17 00:07:40 -0500

    1

---
+++ src/main/java/com/carroGenerico/CarroGenericoApplication.java
@@ -0,0 +1,13 @@
+package com.carroGenerico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarroGenericoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarroGenericoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/carroGenerico/CarroGenericoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.carroGenerico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarroGenericoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
